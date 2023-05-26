965a5afa9076be9468480bacffa50c8cffd40d6b
Débora Lucia Koppe Soares <deboraluciak@hotmail.com>
2023-05-26 00:04:44 -0300

    revisando

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,5 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log(minhaString.trim())
+console.log(minhaString.length)
+console.log(minhaString.replace ("________", "sticioso"))
